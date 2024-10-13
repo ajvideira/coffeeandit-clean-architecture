e3a80e89aad05f8cebc057b62009c90945e1c7bf
Jonathan Alba Videira <jonathan.videira@gmail.com>
2024-10-13 12:39:44 -0300

    Initial structure

---
+++ src/test/java/br/com/ajvideira/cleanarchitecture/CleanarchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ajvideira.cleanarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CleanarchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
